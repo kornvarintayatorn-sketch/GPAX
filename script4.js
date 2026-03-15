91f5b7849909453a788668641cac114964518c80
KornFusion <korn.varintayatorn@gmail.com>
2026-03-15 19:37:48 +0700

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,10 @@
+function caculate(){
+    let g1=parseFloat(document.getElementById("t1").value)||0
+    let g2=parseFloat(document.getElementById("t2").value)||0
+    let g3=parseFloat(document.getElementById("t3").value)||0
+    let g4=parseFloat(document.getElementById("t4").value)||0
+    let g5=parseFloat(document.getElementById("t5").value)||0
+    let g6=parseFloat(document.getElementById("t6").value)||0
+    let gpax= (g1*14.0 + g2*15.5 + g3*16.5 + g4*16.5 + g5*16.5 + g6*14.0) / 93
+    document.getElementById("result").innerText = "GPAX = " + gpax.toFixed(4)
+}
