30b3110525c3d14035ea4ed8f58aa93e3c002dae
KornFusion <korn.varintayatorn@gmail.com>
2026-03-16 10:48:15 +0700

    Update script4.js

--- script4.js
+++ script4.js
@@ -5,6 +5,6 @@ function caculate(){
     let g4=parseFloat(document.getElementById("t4").value)||0
     let g5=parseFloat(document.getElementById("t5").value)||0
     let g6=parseFloat(document.getElementById("t6").value)||0
-    let gpax= (g1*14.0 + g2*15.5 + g3*16.5 + g4*16.5 + g5*16.5 + g6*14.0) / 93
+    let gpax= (g1*14.0 + g2*16 + g3*16.5 + g4*16.5 + g5*15.5 + g6*14.0) / 92.5
     document.getElementById("result").innerText = "GPAX = " + gpax.toFixed(4)
 }
